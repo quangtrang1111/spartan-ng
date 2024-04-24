bc08cad0a48425e6640a8ed033ece49a1ea083fc
quangtrang1111 <huynhquangtrang1994@gmail.com>
2024-04-24 09:55:50 +0700

    Setup SpartanNG in a Angular project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { hlmH1 } from '@spartan-ng/ui-typography-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'spartan-ng-ui';
+  hlmH1 = hlmH1;
 }
